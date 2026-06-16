d6fc19a205690658bfff4ed830a6c32fc4ceb836
Tanishkabadhai <tanishkabadhai@gmail.com>
2026-06-16 13:12:26 +0530

    Added MongoDB Atlas integration and complaint APIs

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from models.complaint import Complaint
 
+from database.connection import complaints_collection
+
 app = FastAPI()
 
 
@@ -12,18 +14,29 @@ def home():
         "message": "CivicLens Backend Running"
     }
 
-complaints = []
-@app.post("/complaints")
-def create_complaint(data: Complaint):
 
-    complaints.append(data.model_dump())
+@app.post("/complaints")
+async def create_complaint(data: Complaint):
+
+    complaint = data.model_dump()
+
+    result = await complaints_collection.insert_one(complaint)
 
     return {
-        "message": "Complaint received",
+        "message": "Complaint saved",
 
-        "data": data
+        "id": str(result.inserted_id)
     }
+
 @app.get("/complaints")
-def get_complaints():
+async def get_complaints():
+
+    complaints = []
+
+    async for complaint in complaints_collection.find():
+
+        complaint["_id"] = str(complaint["_id"])
+
+        complaints.append(complaint)
 
     return complaints
