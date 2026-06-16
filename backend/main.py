c71212feac359448e7b164c8f92b4057cf201c21
Tanishkabadhai <tanishkabadhai@gmail.com>
2026-06-16 11:52:29 +0530

    Add complaint model and complaints API endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from models.complaint import Complaint
+
 app = FastAPI()
 
 
@@ -9,3 +11,19 @@ def home():
     return {
         "message": "CivicLens Backend Running"
     }
+
+complaints = []
+@app.post("/complaints")
+def create_complaint(data: Complaint):
+
+    complaints.append(data.model_dump())
+
+    return {
+        "message": "Complaint received",
+
+        "data": data
+    }
+@app.get("/complaints")
+def get_complaints():
+
+    return complaints
